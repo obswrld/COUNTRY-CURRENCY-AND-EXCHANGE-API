6dbc4c8dbfe5dd6a2a589019c1bfeb0faaf1ba7f
Republic Oba <republicoba1@gmail.com>
2025-10-29 20:33:44 +0100

    new

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 from flask import Flask
+import os
 from models.country_models import db
 from routes.country_routes import country_db
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:LamineYamal10!@localhost:3306/country_exchange_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-
     app.register_blueprint(country_db)
-
     return app
 
 if __name__ == '__main__':
-    app = create_app()
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    create_app().run(host='0.0.0.0', port=port)
