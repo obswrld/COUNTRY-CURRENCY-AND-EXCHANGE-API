f64de5e99210d0ce6fce10a984b2d36ebc944164
Republic Oba <republicoba1@gmail.com>
2025-10-29 21:03:19 +0100

    changes

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 import os
+from dotenv import load_dotenv
 from models.country_models import db
 from routes.country_routes import country_db
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+
+    # ✅ Check if DB_URI is loaded
+    db_uri = os.getenv('DB_URI')
+    print("DEBUG → Loaded DB_URI:", db_uri)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
