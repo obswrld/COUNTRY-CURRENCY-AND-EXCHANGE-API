753364bbcb18e8b9348d4babbae074b3df6da9a2
Republic Oba <republicoba1@gmail.com>
2025-10-29 23:45:53 +0100

    new

--- config/config.py
+++ config/config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'my-secret-key')
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DB_URI', 'mysql+pymysql://root:password@localhost:3306/country_exchange_db'
+        'DB_URI', 'mysql+pymysql://root:LamineYamal%21@localhost:3306/country_exchange_db'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
